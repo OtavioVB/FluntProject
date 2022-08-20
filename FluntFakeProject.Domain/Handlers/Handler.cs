d39dd1798229300b9a81629e13ff0346aaf03e61
OtavioVB <otaviovb.developer@gmail.com>
2022-08-20 11:34:20 -0300

    refactor(handlers): `fixed overwriting method`

--- FluntFakeProject.Domain/Handlers/Handler.cs
+++ FluntFakeProject.Domain/Handlers/Handler.cs
@@ -1,20 +1,69 @@
-﻿namespace FluntFakeProject.Domain.Handlers
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using FluntFakeProject.Domain.ValueObjects;
+
+namespace FluntFakeProject.Domain.Handlers
 {
-    public class Handler
+    public class Handler : Notifiable
     {
         public Response Handle(RequestName request)
         {
+            if (request.IsValid is false)
+            {
+                AppendNotifications(request);
+                throw new Exception("Request name is invalid");
+            }
+
+            var name = new Name(request.FirstName, request.LastName, request.Nickname);
+
+            AppendNotifications(name);
+
+            if (IsValid is false) throw new Exception("Entities or value objects are not valid");
+
             return new Response("Name created");
         }
 
         public Response Handle(RequestAddress request)
         {
+            if (request.IsValid is false)
+            {
+                AppendNotifications(request);
+                throw new Exception("Request address is invalid");
+            }
+
+            var name = new StreetAddress(request.AddressType, request.StreetName, request.HouseNumber, request.Neighborhood, request.Complement, request.IDAddress);
+
+            AppendNotifications(name);
+
+            if (IsValid is false) throw new Exception("Entities or value objects are not valid");
+
             return new Response("Address created");
         }
 
         public Response Handle(RequestIdentity request)
         {
+            if (request.IsValid is false)
+            {
+                AppendNotifications(request);
+                throw new Exception("Request identity is invalid");
+            }
+
+            var name = new Identity(request.GeneralRegistry, request.IndividualCode, request.BirthPlace, request.ShippingDate);
+
+            AppendNotifications(name);
+
+            if (IsValid is false) throw new Exception("Entities or value objects are not valid");
+
             return new Response("Identity created");
+        }
+
+        public Response Handle(RequestName requestName, RequestAddress requestAddress, RequestIdentity requestIdentity)
+        {
+            Handle(requestName);
+            Handle(requestAddress);
+            Handle(requestIdentity);
+
+            return new Response("Person has been created");
         }
     }
 }
